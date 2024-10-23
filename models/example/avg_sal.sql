a5e7223d199edc81a9fde355c272f76d0308fff8
srikad29dbt@gmail.com <srikad29dbt@gmail.com>
2024-10-23 21:43:22 +0000

    today

--- models/example/avg_sal.sql
+++ models/example/avg_sal.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='ephemeral') }}
 
-select deptno,avg(sal) as avg_sal from RAW_DATA.emp group by 1
+select deptno,avg(sal) as avg_sal from DEV.RAW_DATA.EMP_STG group by 1
--- models/less_than_avg.sql
+++ models/less_than_avg.sql
@@ -1,3 +1,3 @@
 select e.*,as1.avg_sal from 
-RAW_DATA.emp e  inner join {{ref('avg_sal')}} as1 
+DEV.RAW_DATA.EMP_STG e  inner join {{ref('avg_sal')}} as1 
 on (as1.deptno=e.deptno) and e.sal<as1.avg_sal
