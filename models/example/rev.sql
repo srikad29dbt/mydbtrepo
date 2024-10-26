11620cef613a01986c3df87ba8ea2e28b49f08a3
srikad29dbt@gmail.com <srikad29dbt@gmail.com>
2024-10-26 03:38:12 +0000

    l;kjjk

--- models/example/rev.sql
+++ models/example/rev.sql
@@ -1,3 +1,4 @@
-select prod_name, sum(quantity*unit_price) as revenue  from CURATED_DATA.PRODUCTS P inner join CURATED_DATA.SALES S
+select prod_name, sum(quantity*unit_price) as revenue  
+from CURATED_DATA.PRODUCTS P inner join CURATED_DATA.SALES S
 on (P.prod_id=S.prod_id)
 group by prod_name
