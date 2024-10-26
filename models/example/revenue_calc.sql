c95cee1b5de59da53e5b0d76146b21a70f6acf38
srikad29dbt@gmail.com <srikad29dbt@gmail.com>
2024-10-26 02:03:13 +0000

    sources

--- models/example/revenue_calc.sql
+++ models/example/revenue_calc.sql
@@ -2,6 +2,7 @@
 
 }}
 
-select prod_name, sum(quantity*unit_price) as revenue  from RAW_DATA.PRODUCTS_STG P inner join RAW_DATA.SALES_STG S
+select prod_name, sum(quantity*unit_price) as revenue  from 
+{{source('RAW_DATA','products_stg')}} P inner join   {{source('RAW_DATA','sales_stg')}} S
 on (P.prod_id=S.prod_id)
 group by prod_name
